e4d9757510db90841a77b55c26726d4551478d97
assu10 <juhyun10.lee@gmail.com>
2021-09-12 20:06:15 +0900

    Typescript 기본 - 4.3. 모듈 (export, import) 에서 외부패키지 import

--- chap02/src/index.ts
+++ chap02/src/index.ts
@@ -1,6 +1,12 @@
 import IPerson from "./person/IPerson";
 import Person, {makePerson} from "./person/Person";     // Person 은 export default, makePerson 은 export
+import Chance from 'chance'
+import * as R from 'ramda'
 
+const chance = new Chance()
+let persons: IPerson[] = R.range(0, 3)
+    .map((n: number) => new Person(chance.name(), chance.age()))
+console.log(persons)
 
 const testMakePerson = (): void => {
     let assu: IPerson = makePerson('assu')
