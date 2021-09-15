a8f85af658226ef03af0e5e8850d8f3b0b9d1ca8
assu10 <juhyun10.lee@gmail.com>
2021-09-15 23:06:24 +0900

    Typescript - 객체, 타입 - 3.2. 인터페이스 구현 (implements)

--- chap02/src/index.ts
+++ chap02/src/index.ts
@@ -1,18 +1,17 @@
-// 클래스
-class Person1 {
+// 인터페이스
+interface IPerson {
     name: string
     age?: number
 }
 
-let assu1: Person1 = new Person1();
-assu1.name = 'assu'
-assu1.age = 20;
+class Person2 implements IPerson {
+    name: string
+    age: number
+}
 
-console.log(assu1)      // Person1 { name: 'assu', age: 20 }
-
-class Person2 {
+class Person3 implements IPerson {
     constructor(public name: string, public age?: number) { }
 }
 
-let assu2: Person2 = new Person2('assu2', 20)
-console.log(assu2)  // Person2 { name: 'assu2', age: 20 }
+let assu3: Person3 = new Person3('assu')
+console.log(assu3)      // Person3 { name: 'assu', age: undefined }
--- chap02/src/utils/makePerson.ts
+++
@@ -1,7 +0,0 @@
-export function makePerson(name: string, age:number) {
-    return {name: name, age: age}
-}
-
-export function testMakePerson() {
-    console.log(makePerson('Assu', 22), makePerson('jhlee', 23))
-}
