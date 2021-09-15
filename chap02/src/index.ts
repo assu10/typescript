f9958c5f46278b568585388b4ca6ae0b50de397e
assu10 <juhyun10.lee@gmail.com>
2021-09-15 23:29:33 +0900

    Typescript - 객체, 타입 - 3.4. static 속성

--- chap02/src/index.ts
+++ chap02/src/index.ts
@@ -1,17 +1,7 @@
-// 추상 클래스
-abstract class AbstractPerson {
-    abstract name: string
-    constructor(public age?: number) { }
+// static 속성
+class Person {
+    static addr: string = 'seoul'
 }
 
-class Person extends AbstractPerson {
-    constructor(public name: string, age?: number) {
-        super(age);
-    }
-}
-
-let assu1: Person = new Person('assu1')
-let assu2: Person = new Person('assu2', 20)
-
-console.log(assu1)  // Person { age: undefined, name: 'assu1' }
-console.log(assu2)  // Person { age: 20, name: 'assu2' }
+let assuAddr = Person.addr
+console.log(assuAddr)
