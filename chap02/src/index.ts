454d8400799e3e54ab1d87bf98e7eb29ffa4cfb0
assu10 <juhyun10.lee@gmail.com>
2021-09-17 19:35:04 +0900

    Typescript - 객체, 타입 -  5.1. 타입 변환 (type conversion)

--- chap02/src/index.ts
+++ chap02/src/index.ts
@@ -1,23 +1,6 @@
-// 잔여 연산
-let address: any = {
-    country: 'Korea',
-    city: 'seoul',
-    address1: 'Jam-sil',
-    address2: '311-10'
-}
-const {country, city, ...detail} = address
-console.log(country)    // Korea
-console.log(detail)     // { address1: 'Jam-sil', address2: '311-10' }
+// 타입 변환
+let person: object = {name: 'assu', age: 20};
+//person.name = 'assu2';  // TS2339: Property 'name' does not exist on type 'object'.
 
-
-// 전개 연산
-let coord = {...{x: 0}, ...{y: 0}}
-console.log(coord)  //{ x: 0, y: 0 }
-
-let part1 = {name: 'assu'}
-let part2 = {age: 20}
-let part3 = {city: 'suwon', country: 'kr'}
-let part4 = {name: 'reAssu'}
-let merged = {...part1, ...part2, ...part3, ...part4}
-
-console.log(merged) // { name: 'reAssu', age: 20, city: 'suwon', country: 'kr' }
+(<{name: string}>person).name = 'assu2';
+console.log(person);    // { name: 'assu2', age: 20 }
