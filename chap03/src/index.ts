29edd5e619740a52d91f35f0a75ba4fd35cec4a0
assu10 <juhyun10.lee@gmail.com>
2021-09-19 19:28:25 +0900

    Typescript - 함수, 메서드 -  1. 함수 선언

---
+++ chap03/src/index.ts
@@ -0,0 +1,11 @@
+// 함수 선언문
+function add(a: number, b: number): number {
+    return a+b;
+}
+
+// void type
+function printMe(name: string, age: number) : void {
+    console.log(`name: ${name}, age: ${age}`);
+}
+
+printMe('assu', 20);
