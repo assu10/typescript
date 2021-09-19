bf3b1c24846f943dece7cec7b04f16fd9d3fbc0f
assu10 <juhyun10.lee@gmail.com>
2021-09-19 20:49:43 +0900

    Typescript - 함수, 메서드 -  1.3. 선택적 매개변수

--- chap03/src/index.ts
+++ chap03/src/index.ts
@@ -1,6 +1,15 @@
-// 타입 별칭
-type stringNumberFunc = (p1: string, p2: number) => void;
+// 선택적 매개변수
+type OptionalArgFunc = (p1: string, p2?: number) => void;
 
-let f: stringNumberFunc = function(a: string, b: number): void { }
-let g: stringNumberFunc = function(c: string, d: number): void { }
-let h: stringNumberFunc = function(e: number) : void { }    // TS2322: Type '(e: number) => void' is not assignable to type 'stringNumberFunc'.
+function fn(p1: string, p2?: number): void {
+    console.log(`p2: ${p2}`);
+}
+
+let fn2: OptionalArgFunc = function(a: string, b?: number) : void {
+    console.log(`b: ${b}`);
+}
+
+fn('assu', 2);   // p2: 2
+fn('assu');          // p2: undefined
+fn2('assu', 2);  // b: 2
+fn2('assu');         // b: undefined
