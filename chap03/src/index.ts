be44e3b9e906020673f7808ad1ea94c27fef9377
assu10 <juhyun10.lee@gmail.com>
2021-09-20 01:59:43 +0900

    Typescript - 함수, 메서드 -  3.1. 실행문과 표현식문

--- chap03/src/index.ts
+++ chap03/src/index.ts
@@ -1,18 +1,4 @@
-// 익명 함수
+// 화살표 함수(`=>`)와 표현식
 
-let funcExpression = (function(a: number, b: number): number {
-    return a+b;
-})(1, 2);
-
-let funcExpression2 =
-(function(a: number, b: number): number {
-    return a+b;
-})
-(1, 2);     // 곧바로 함수 호출 연산자를 만나므로 eager evaluation 을 적용해 3이라는 값을 만들어냄
-
-console.log(funcExpression);    // 3
-
-let funcExpression3 = (function(): void {
-    console.log('hello');
-})();   // hello
-console.log(funcExpression3);   // undefined
+const arrow1 = (a: number, b: number): number => { return a+b }
+const arrow2 = (a: number, b: number): number => a+b
