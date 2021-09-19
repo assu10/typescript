bdc6d8925a52e3baaa2337c3c9daeb4745702db5
assu10 <juhyun10.lee@gmail.com>
2021-09-19 21:16:35 +0900

    Typescript - 함수, 메서드 -  2.3. 익명 함수 (anonymous function)

--- chap03/src/index.ts
+++ chap03/src/index.ts
@@ -1,6 +1,18 @@
-// 함수 호출 연산자
+// 익명 함수
 
-let funcExpression = function(a: number, b: number): number {
+let funcExpression = (function(a: number, b: number): number {
     return a+b;
-};
-let value = funcExpression(1, 2);
+})(1, 2);
+
+let funcExpression2 =
+(function(a: number, b: number): number {
+    return a+b;
+})
+(1, 2);     // 곧바로 함수 호출 연산자를 만나므로 eager evaluation 을 적용해 3이라는 값을 만들어냄
+
+console.log(funcExpression);    // 3
+
+let funcExpression3 = (function(): void {
+    console.log('hello');
+})();   // hello
+console.log(funcExpression3);   // undefined
