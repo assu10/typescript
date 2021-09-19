c564641081b30190878bcbb6709338e4e643a9b7
assu10 <juhyun10.lee@gmail.com>
2021-09-19 19:48:29 +0900

    Typescript - 함수, 메서드 -  1.2. 타입 별칭 (type alias)

--- chap03/src/index.ts
+++ chap03/src/index.ts
@@ -1,9 +1,6 @@
-// 함수 시그니처
-function printMe(name: string, age: number) : void {
-    console.log(`name: ${name}, age: ${age}`);
-}
+// 타입 별칭
+type stringNumberFunc = (p1: string, p2: number) => void;
 
-let printMeSignature: (a: string, b: number) => void = function (name: string, age: number): void {
-    console.log(`name: ${name}, age: ${age}`);
-}
-printMeSignature('assu', 20);
+let f: stringNumberFunc = function(a: string, b: number): void { }
+let g: stringNumberFunc = function(c: string, d: number): void { }
+let h: stringNumberFunc = function(e: number) : void { }    // TS2322: Type '(e: number) => void' is not assignable to type 'stringNumberFunc'.
