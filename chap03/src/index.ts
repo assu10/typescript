f0408830670eb0c0b207452f6cf75d5b8b9f9c9f
assu10 <juhyun10.lee@gmail.com>
2021-09-20 02:15:47 +0900

    Typescript - 함수, 메서드 -  3.2. 표현식문 스타일의 화살표 함수 구현

--- chap03/src/index.ts
+++ chap03/src/index.ts
@@ -1,4 +1,10 @@
-// 화살표 함수(`=>`)와 표현식
+// 표현식문 스타일의 화살표 함수 구현
 
-const arrow1 = (a: number, b: number): number => { return a+b }
-const arrow2 = (a: number, b: number): number => a+b
+// 일반 함수
+function isGreater(a: number, b:number): boolean {
+    return a > b;
+}
+
+// 표현식문 스타일의 화살표 함수
+const isGreater2 = (a: number, b: number): boolean => a > b;
+const isGreater3 = (a: number, b: number): boolean => { return a > b }; // return 키워드를 사용하려면 중괄호로 복합 실행문을 만든 후 그 안에 사용
