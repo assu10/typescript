0fe5d7e79bdcb842904938ab4aa43c37de12ddeb
assu10 <juhyun10.lee@gmail.com>
2021-09-20 17:36:18 +0900

    Typescript - 함수, 메서드 -  6.3. 클래스 메서드 구문

--- chap03/src/index.ts
+++ chap03/src/index.ts
@@ -5,5 +5,8 @@ import {A, B} from "./A";
 let a: A = new A
 a.method()  // value: 1
 
-let b: B = new B
-b.method()  // // value: 1
+let b: B = new B()
+let b2: B = new B(3)
+
+b.method()  // value: 1
+b2.method()  // value: 3
