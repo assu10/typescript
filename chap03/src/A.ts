2501c6d4b504cb71d5e757157e59606a0d5793be
assu10 <juhyun10.lee@gmail.com>
2021-09-20 17:34:15 +0900

    Typescript - 함수, 메서드 -  6.3. 클래스 메서드 구문

--- chap03/src/A.ts
+++ chap03/src/A.ts
@@ -1,8 +1,17 @@
-// 메서드
+// 클래스 메서드 구문
 
 export class A {
     value: number = 1
     method: () => void = function(): void {
+        console.log(`value: ${this.value}`)
+    }
+}
+
+
+// 함수표현식을 담는 속성은 function 키워드 생략
+export class B {
+    constructor(public value: number = 1) { }
+    method(): void {
         console.log(`value: ${this.value}`)
     }
 }
--- chap03/src/index.ts
+++ chap03/src/index.ts
@@ -1,6 +1,9 @@
-// 메서드
+// 클래스 메서드 구문
 
-import {A} from "./A";
+import {A, B} from "./A";
 
 let a: A = new A
 a.method()  // value: 1
+
+let b: B = new B
+b.method()  // // value: 1
