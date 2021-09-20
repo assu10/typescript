514e208bfe1240591f5c27262b9871b6ae08b7f9
assu10 <juhyun10.lee@gmail.com>
2021-09-20 16:21:11 +0900

    Typescript - 함수, 메서드 -  5.4. 매개변수에 비구조화 할당문 사용

--- chap03/src/index.ts
+++ chap03/src/index.ts
@@ -1,17 +1,14 @@
-// 객체를 반환하는 화살표 함수
+// 매개변수에 비구조화 할당문 사용
 
 export type Person = { name: string, age: number };
 
-// 객체 생성 시 값을 생략하는 타입스크립트 구문
-const makePerson = (name: string, age: number = 10): Person => {
-    const person = { name, age }      // const person = { name: name, age: age} 의 단축 표현
-    return person
-}
-
-console.log(makePerson('assu'))     // { name: 'assu', age: 10 }
-
-// 화살표 함수에서 객체를 반환하고자 할 때 아래와 같이 하면 컴파일러는 중괄호를 객체가 아닌 복합 실행문으로 해석함
-const makePersonBad = (name: string, age: number = 10): Person => { name, age } // 오류
-
 // 컴파일러가 중괄호를 객체로 해석하게 하려면 객체를 소괄호 ( ) 로 감싸주어야 함
-const makePersonGood = (name: string, age: number = 10): Person => ({ name, age })
+const makePerson1 = (name: string, age: number = 10): Person => ({ name, age })
+
+// 매개변수에 비구조화 할당문 적용
+const makePerson2 = ({ name, age }: Person) => ({ name, age })
+const makePerson3 = ({ name, age }: Person): void =>
+    console.log(`name: ${name}, age: ${age}`)
+
+makePerson2({name: 'assu', age: 10})
+makePerson3({name: 'assu', age: 20})   // name: assu, age: 20
