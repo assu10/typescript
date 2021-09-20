37b8f0413fb1e5fc4e16fbc58aa7658d75d938aa
assu10 <juhyun10.lee@gmail.com>
2021-09-20 14:00:22 +0900

    Typescript - 함수, 메서드 -  4.1. 콜백 함수

--- chap03/src/index.ts
+++ chap03/src/index.ts
@@ -1,10 +1,5 @@
-// 표현식문 스타일의 화살표 함수 구현
+// 콜백 함수
 
-// 일반 함수
-function isGreater(a: number, b:number): boolean {
-    return a > b;
-}
+import {init} from "./init";
 
-// 표현식문 스타일의 화살표 함수
-const isGreater2 = (a: number, b: number): boolean => a > b;
-const isGreater3 = (a: number, b: number): boolean => { return a > b }; // return 키워드를 사용하려면 중괄호로 복합 실행문을 만든 후 그 안에 사용
+init(() => console.log('custom init finished.') );
