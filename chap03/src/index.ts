5d7099e2384d05ddee14a1d2593808500f3bb365
assu10 <juhyun10.lee@gmail.com>
2021-09-20 15:43:48 +0900

    Typescript - 함수, 메서드 -  5.2. 객체 생성 시 값을 생략하는 타입스크립트 구문

--- chap03/src/index.ts
+++ chap03/src/index.ts
@@ -1,4 +1,4 @@
-// 매개변수 기본값 설정
+// 객체 생성 시 값을 생략하는 타입스크립트 구문
 
 export type Person = { name: string, age: number };
 
@@ -7,5 +7,8 @@ export const makePerson = (name: string, age: number = 10): Person => {
     return person;
 }
 
-console.log(makePerson('assu'));            // { name: 'assu', age: 10 }
-console.log(makePerson('assu', 20));   // { name: 'assu', age: 20 }
+// 객체 생성 시 값을 생략하는 타입스크립트 구문
+const makePerson2 = (name: string, age: number = 10): Person => {
+    const person2 = { name, age };      // const person = { name: name, age: age} 의 단축 표현
+    return person2;
+}
