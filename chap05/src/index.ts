157a87fe410c9bb639383489cfbb2f829d5646bc
assu10 <juhyun10.lee@gmail.com>
2021-09-21 22:46:16 +0900

    Typescript - 배열, 튜플 -  3.2. 깊은 복사, 얕은 복사

--- chap05/src/index.ts
+++ chap05/src/index.ts
@@ -1,5 +1,17 @@
-// 타입 수정자 readonly
+// 깊은 복사, 얕은 복사
 
-function pure(array: readonly number[]) {
-    array.push(1)  // TS2339: Property 'push' does not exist on type 'readonly number[]'.
-}
+// 깊은 복사
+let original: number = 1
+let copied = original
+
+copied += 2
+
+console.log(original, copied)   // 1 3
+
+
+// 얕은 복사
+const originalArr = [1,2,3,4]
+const shallowCopiedArr = originalArr
+
+shallowCopiedArr[0] = 10
+console.log(originalArr, shallowCopiedArr)  // [ 10, 2, 3, 4 ] [ 10, 2, 3, 4 ]
