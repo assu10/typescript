fa32e3444f59b7b321a15035c862d02e76dc5740
assu10 <juhyun10.lee@gmail.com>
2021-09-21 18:23:09 +0900

    Typescript - 배열, 튜플 -  1.3. for...in, for...of

--- chap05/src/index.ts
+++ chap05/src/index.ts
@@ -1,5 +1,19 @@
-// 배열의 비구조화
+// for...in, for...of
 
-let array = [1,2,3,4,5]
-let [first, second, third, ...rest] = array
-console.log(first, second, third, rest)     // 1 2 3 [ 4, 5 ]
+// 배열인 경우 for...in
+let array = ['assu', 'jh', 'lee']
+for (let index in array) {
+    const name = array[index]
+    console.log(`${index} - ${name}`)
+}
+
+// 객체인 경우 for...in
+let obj = {name: 'assu', age: 20}
+for (let prop in obj) {
+    console.log(`${prop} ${obj[prop]}`)     // tsconfig.json 의 stric 이 false 이어야 함
+}
+
+// 배열인 경우 for...of
+for (let name of array) {
+    console.log(name)
+}
