e48aea2db4cb62495cdf2d1b80a28844172a3cc7
assu10 <juhyun10.lee@gmail.com>
2021-09-21 21:13:54 +0900

    Typescript - 배열, 튜플 -  2.2. map 메서드 - 제곱 구하기

--- chap05/src/index.ts
+++ chap05/src/index.ts
@@ -1,11 +1,6 @@
-// filter 메서드 - 배열 반으로 나누기
+// map 메서드 - 제곱 구하기
 
 import {range} from "./range";
 
-const array: number[] = range(1, 10+1)
-const half = array.length / 2
-
-let belowHalf: number[] = array.filter((value, index) => index < half)
-let overHalf: number[] = array.filter((value, index) => index >= half)
-
-console.log(belowHalf, overHalf)    // [ 1, 2, 3, 4, 5 ] [ 6, 7, 8, 9, 10 ]
+let squared: number[] = range(1, 5+1).map((value: number) => value * value)
+console.log(squared)    // [ 1, 4, 9, 16, 25 ]
