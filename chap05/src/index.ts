00518a2a156568bfcdaa7a744aba245874a30fa5
assu10 <juhyun10.lee@gmail.com>
2021-09-21 19:01:53 +0900

    Typescript - 배열, 튜플 -  1.5. 제네릭 방식 타입 추론

--- chap05/src/index.ts
+++ chap05/src/index.ts
@@ -1,19 +1,8 @@
-// 제네릭 방식 타입
+// 제네릭 방식 타입 추론
 
-let numArray: number[] = [1, 2, 3]
-let strArray: string[] = ['one', 'two'];
-
-type IPerson = {name: string, age?: number}
-let personArray: IPerson[] = [{name: 'assu'}, {name: 'jhlee', age: 20}]
-
-const arrayLength = (array: string[]) => array.length
-const arrayGenericLength = <T>(array: T[]): number => array.length
-const isEmpty = <T>(array: T[]): boolean => arrayGenericLength(array) == 0
+let fn = <T>(n: T): T => n
 
 console.log(
-    arrayGenericLength(numArray),   // 3
-    arrayGenericLength(strArray),   // 2
-    arrayGenericLength(personArray),    // 2
-    isEmpty([]),    // true
-    isEmpty([1])    // false
+    fn<boolean>(true),  // true
+    fn(true)        // true
 )
