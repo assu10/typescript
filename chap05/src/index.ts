5e0250e9b549cfc7b5e3ada74d1bf91cbbb8be95
assu10 <juhyun10.lee@gmail.com>
2021-09-21 22:35:34 +0900

    Typescript - 배열, 튜플 -  3.1. 타입 수정자 readonly

--- chap05/src/index.ts
+++ chap05/src/index.ts
@@ -1,10 +1,5 @@
-// reduce 메서드
+// 타입 수정자 readonly
 
-import {range} from "./range";
-
-let reduceSum: number = range(1, 100+1).reduce((result: number, value: number) => result + value, 0)
-console.log(reduceSum)  // 5050
-
-// 곱하기는 0 을 곱하면 안되니까 두 번째 인수에 1을 넣어준다.
-let reduceMultiply: number = range(1, 10+1).reduce((result: number, value: number) => result * value, 1)
-console.log(reduceMultiply) // 3628800
+function pure(array: readonly number[]) {
+    array.push(1)  // TS2339: Property 'push' does not exist on type 'readonly number[]'.
+}
