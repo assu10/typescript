b4f227b01ebc53cc56db7ac39cc1fe6c3b01df09
assu10 <juhyun10.lee@gmail.com>
2021-09-24 19:33:50 +0900

    Typescript - 반복기, 생성기 -  `yield` 반환값

--- chap06/src/index.ts
+++ chap06/src/index.ts
@@ -1,16 +1,27 @@
-// yield*` 키워드
+// `yield` 반환값
 
-function* gen12() {
-    yield 1
-    yield 2
+function* gen() {
+    let count: number = 5
+    let select: number = 0  // 처음엔 항상 0 이 출력
+    while (count--) {
+        select = yield `you select ${select}`   // yield 연산자의 반환값을 select 에 저장
+    }
 }
 
-function* gen12345() {
-    yield* gen12()
-    yield* [3,4]
-    yield 5
-}
+const random = (max: number, min: number = 0) => Math.round(Math.random() * (max-min)) + min
 
-for (let value of gen12345()) {
-    console.log(value)  // 1 2 3 4 5
+const iter = gen()
+while (true) {
+    const {value, done} = iter.next(random(10, 1))  // yield 연산자의 반환값은 반복기의 next 메서드 호출 시 매개변수에 전달하는 값
+    if (done) {
+        break
+    }
+    console.log(value)
 }
+/*
+you select 0
+you select 4
+you select 8
+you select 10
+you select 7
+*/
