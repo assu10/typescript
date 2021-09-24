7e26a742910d655de0b648afd6956f2ccc298285
assu10 <juhyun10.lee@gmail.com>
2021-09-24 14:52:52 +0900

    Typescript - 반복기, 생성기 -  `for...of` 구문과 `[Symbol.iterator]` 메서드 - 오류

--- chap06/src/index.ts
+++ chap06/src/index.ts
@@ -1,13 +1,9 @@
-// 반복기와 반복기 제공자
+// `for...of` 구문과 `[Symbol.iterator]` 메서드 - 오류
 
 import { createRangeIterable } from "./createRangeIterable";
 
 const iterator = createRangeIterable(1, 3+1)    // 반복기는 현재 동작하지 않음
 
-while(true) {
-    const {value, done} = iterator.next();  // 반복기 동작
-    if (done) {
-        break
-    }
-    console.log(value)  // 1 2 3
+for (let value of iterator) {   // TS2488: Type '{ next(): { value: number | undefined; done: boolean; }; }' must have a '[Symbol.iterator]()' method that returns an iterator.
+    console.log(value)
 }
