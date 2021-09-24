bcba98d3839ce50ee1cd1668605b8102cfed7dc5
assu10 <juhyun10.lee@gmail.com>
2021-09-24 11:55:28 +0900

    Typescript - 반복기, 생성기 -  1.1. 반복기(`iterator`)와 반복기 제공자(`iterable`)

---
+++ chap06/src/index.ts
@@ -0,0 +1,11 @@
+// for...of
+
+const numArray: number[] = [1, 2, 3]
+for (let value of numArray) {
+    console.log(value)  //  1 2 3
+}
+
+const strArray: string[] = ['a', 'b', 'c']
+for (let value of strArray) {
+    console.log(value)  // a b c
+}
