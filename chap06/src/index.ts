c162657618644237b98c8fe0c68d19c7762f793e
assu10 <juhyun10.lee@gmail.com>
2021-09-24 18:37:41 +0900

    Typescript - 반복기, 생성기 -  2.2. `yield` 키워드

--- chap06/src/index.ts
+++ chap06/src/index.ts
@@ -1,24 +1,18 @@
-// `yield` 키워드
+// 반복기 제공자의 메서드로 동작하는 생성기 구현
 
-function* rangeGenerator(from: number, to: number) {
-    let value: number = from
-    while (value < to) {
-        yield value++
+class IterableUsingGenerator<T> implements Iterable<T> {
+    constructor(private values: T[] = [], private currentIndex: number = 0) { }
+    [Symbol.iterator] = function* () {
+        while (this.currentIndex < this.values.length) {        // "noImplicitThis": false
+            yield this.values[this.currentIndex++]
+        }
     }
 }
 
-// while 패턴으로 동작하는 생성기
-let iterator = rangeGenerator(1, 3+1)
-while (1) {
-    const {value, done} = iterator.next()
-    if (done) {
-        break
-    }
-    console.log(value)  // 1 2 3
+for (let item of new IterableUsingGenerator([1, 2, 3])) {
+    console.log(item)   //  1 2 3
 }
 
-
-// for...of 패턴으로 동작하는 생성기
-for (let value of iterator) {
-    console.log(value) // 위에서 3이 되었으므로 아무것도 출력안됨
+for (let item of new IterableUsingGenerator(['hello', 'world', '!'])) {
+    console.log(item)   // hello world !
 }
