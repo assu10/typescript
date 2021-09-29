180071bb4912adb99a46ab6ae65040bc186fa223
assu10 <juhyun10.lee@gmail.com>
2021-09-29 21:32:39 +0900

    Typescript - ramda 라이브러리 (1)  - `R.pipe` 함수

--- chap09/src/index.ts
+++ chap09/src/index.ts
@@ -1,14 +1,9 @@
-// 람다 기본 사용법
+//import { range } from "ramda";
+import * as R from 'ramda'
 
-import { range } from "ramda";
+const numbers: number[] = R.range(1, 9+1);
+R.pipe(
+    R.tap(n => console.log(n))  // [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+)(numbers);
 
-console.log(
-    range(1, 9+1)
-);
-
-/*
-[
-    1, 2, 3, 4, 5,
-    6, 7, 8, 9
-]
-*/
+//R.tap(n => console.log(n))(numbers);    // [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
