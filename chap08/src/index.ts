5d8e9097cb371f9910757a46ace253863fc50b72
assu10 <juhyun10.lee@gmail.com>
2021-09-29 18:41:40 +0900

    Typescript - 함수 조합 - 4.3. pipe 와 compose 함수

--- chap08/src/index.ts
+++ chap08/src/index.ts
@@ -1,13 +1,17 @@
 // compose 함수
 
-import { compose } from "./compose";
 import { f, g, h } from "./fgh"
+import { compose } from "./compose";
+import { pipe } from "./pipe";
 
 const composedFGH = compose(h, g, f);
 console.log(composedFGH('x'));  // h(g(f(x)))
 
 
 const inc = (x: number) => x + 1;
-
 const composedInc = compose(inc, inc, inc);
 console.log(composedInc(1));    // 4
+
+
+const pipedFGH = pipe(h, g, f);
+console.log(pipedFGH('x'));     // f(g(h(x)))
