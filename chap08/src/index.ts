c338cfd176f703919cf8bd1a47ffbb03596f2bd6
assu10 <juhyun10.lee@gmail.com>
2021-09-29 15:41:10 +0900

    Typescript - 함수 조합 - 3.1. 고차 함수 (high-order function)

--- chap08/src/index.ts
+++ chap08/src/index.ts
@@ -1,1 +1,23 @@
-console.log(1)
+// 고차 함수
+
+// 고차 함수의 시그니처
+type FirstOrderFunc<T, R> = (p1: T) => R
+type SecondOrderFunc<T, R> = (p1: T) => FirstOrderFunc<T, R>
+type ThirdOrderFunc<T, R> = (p1: T) => SecondOrderFunc<T, R>
+
+// number 타입의 값을 반환하므로 1차 함수
+const add1: FirstOrderFunc<number, number> = (x: number): number => x + 1;
+//console.log(add1(1));    // 2
+
+// FirstOrderFunc<number, number> 을 반환하므로 2차 고차 함수
+const add2: SecondOrderFunc<number, number> =
+    (x: number): FirstOrderFunc<number, number> =>
+    (y: number): number => x + y;
+//console.log(add2(1)(2));     // 3
+
+// SecondOrderFunc<number, number> 을 반환하므로 3차 고차 함수
+const add3: ThirdOrderFunc<number, number> =
+    (x: number): SecondOrderFunc<number, number> =>
+        (y: number): FirstOrderFunc<number, number> =>
+            (z: number): number => x + y + z;
+//console.log(add3(1)(2)(3)); // 6
