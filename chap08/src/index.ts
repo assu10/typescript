d5f2a8456c262b2c68ab7e4cd1543553fff452c9
assu10 <juhyun10.lee@gmail.com>
2021-09-29 19:42:08 +0900

    Typescript - 함수 조합 - 4.5. 포인트가 없는 함수 - reduce

--- chap08/src/index.ts
+++ chap08/src/index.ts
@@ -1,14 +1,18 @@
-// 포인트가 없는 함수 - map
+// 포인트가 없는 함수 - reduce
 
-import { pipe } from "./pipe";
+import {pipe} from "./pipe";
 
 const map = (f: any) => (a: any) => a.map(f);
-//const map2 = <T, R>(f: (T) => R) => (a: T[]): R[] => a.map(f)
-
 const square = (value: any) => value * value;
-const squareMap = map(square);  // 아래처럼 굳이 a 를 지정하지 않아도 된다
-//const squareMap2 = a => map(square);
+const squareMap = map(square);
 
-const fourSquare = pipe(squareMap, squareMap);
 
-console.log(fourSquare([3, 4]));    // [81, 256]
+const reduce = (f: any, initValue: any) => (a: any) => a.reduce(f, initValue);
+// const reduce2 = <T>(f: (sum: T, value: T) => T, initValue: T) => (a: T[]): T => a.reduce(f, initValue);
+
+const sum = (result: number, value: number) => result + value;
+const sumArray = reduce(sum, 0);
+
+const pitagoras = pipe(squareMap, sumArray, Math.sqrt);
+
+console.log(pitagoras([3,4]));  // 5 (3*3 + 4*4 의 제곱근)
