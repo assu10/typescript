c3bd2ae6205b403da171bd19731ea2e83a775b6e
assu10 <juhyun10.lee@gmail.com>
2021-09-29 16:16:21 +0900

    Typescript - 함수 조합 - 3.3. 클로저 (closure) - 자유 변수

--- chap08/src/index.ts
+++ chap08/src/index.ts
@@ -1,39 +1,21 @@
-// 고차 함수
+// 클로저 - 자유 변수
 
-// 고차 함수의 시그니처
-type FirstOrderFunc<T, R> = (p1: T) => R
-type SecondOrderFunc<T, R> = (p1: T) => FirstOrderFunc<T, R>
-type ThirdOrderFunc<T, R> = (p1: T) => SecondOrderFunc<T, R>
+function add(x: number): (p1: number) => number {
+    return function(y: number): number {
+        return x + y;
+    }
+}
 
-// number 타입의 값을 반환하므로 1차 함수
-const add1: FirstOrderFunc<number, number> = (x: number): number => x + 1;
-//console.log(add1(1));    // 2
+// 오류
+/*
+function add2(x: number): number {
+    return function(y: number): number {    // TS2322: Type '(y: number) => number' is not assignable to type 'number'.
+        return x + y;
+    }
+}*/
 
-// FirstOrderFunc<number, number> 을 반환하므로 2차 고차 함수
-const add2: SecondOrderFunc<number, number> =
-    (x: number): FirstOrderFunc<number, number> =>
-    (y: number): number => x + y;
-//console.log(add2(1)(2));     // 3
+const add1 = add(1);
+console.log(add1);  // [Function (anonymous)]
 
-// SecondOrderFunc<number, number> 을 반환하므로 3차 고차 함수
-const add3: ThirdOrderFunc<number, number> =
-    (x: number): SecondOrderFunc<number, number> =>
-        (y: number): FirstOrderFunc<number, number> =>
-            (z: number): number => x + y + z;
-//console.log(add3(1)(2)(3)); // 6
-
-
-// 부분 적용 함수
-const add2_1: FirstOrderFunc<number, number> = add2(1);
-console.log(
-    add2_1(2),      // 3
-    add2(1)(2)  // 3
-);
-
-const add3_1: SecondOrderFunc<number, number> = add3(1);
-const add2_2: FirstOrderFunc<number, number> = add3_1(2);
-console.log(
-    add2_2(3),              // 6
-    add3_1(2)(3),       // 6
-    add3(1)(2)(3)   // 6
-);
+const result = add1(2);
+console.log(result) // 3
