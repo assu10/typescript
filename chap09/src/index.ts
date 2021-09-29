a3b910c33dec737b937bf0345461eb892b24c32b
assu10 <juhyun10.lee@gmail.com>
2021-09-29 21:55:07 +0900

    Typescript - ramda 라이브러리 (1)  -  3.4. 포인트가 없는 함수

--- chap09/src/index.ts
+++ chap09/src/index.ts
@@ -1,9 +1,23 @@
 //import { range } from "ramda";
 import * as R from 'ramda'
 
-const numbers: number[] = R.range(1, 9+1);
-R.pipe(
-    R.tap(n => console.log(n))  // [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
-)(numbers);
+// dump 는 포인트가 없는 함수의 전형적인 모습
+const dump = R.pipe(
+    R.tap(n => console.log(n))
+);
 
-//R.tap(n => console.log(n))(numbers);    // [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+dump(R.range(1, 9+1))   // [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+
+// TS2322: Type 'unknown' is not assignable to type 'T[]'.
+/*
+const dump2 = <T>(array: T[]): T[] => R.pipe(
+    R.tap(n => console.log(n))
+)(array)
+*/
+
+// 타입 단언 사용
+const dump2 = <T>(array: T[]): T[] => R.pipe(
+    R.tap(n => console.log(n))
+)(array) as T[];
+
+dump2(R.range(1, 9+1))   // [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
