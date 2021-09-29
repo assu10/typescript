4ab69afc507334366ca33ccdb54a5c5bcd29d3ae
assu10 <juhyun10.lee@gmail.com>
2021-09-29 22:00:35 +0900

    Typescript - ramda 라이브러리 (1)  -  3.5. 자동 커리

--- chap09/src/index.ts
+++ chap09/src/index.ts
@@ -1,23 +1,8 @@
 //import { range } from "ramda";
 import * as R from 'ramda'
 
-// dump 는 포인트가 없는 함수의 전형적인 모습
-const dump = R.pipe(
-    R.tap(n => console.log(n))
-);
-
-dump(R.range(1, 9+1))   // [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
-
-// TS2322: Type 'unknown' is not assignable to type 'T[]'.
-/*
-const dump2 = <T>(array: T[]): T[] => R.pipe(
-    R.tap(n => console.log(n))
-)(array)
-*/
-
-// 타입 단언 사용
-const dump2 = <T>(array: T[]): T[] => R.pipe(
-    R.tap(n => console.log(n))
-)(array) as T[];
-
-dump2(R.range(1, 9+1))   // [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+console.log(
+    R.add(1, 2),    // 3, 매개변수가 2개인 일반함수
+    R.add(1)(2)     // 3, 2차 고차 함수
+    //R.add(1, 2, 3), // TS2554: Expected 1-2 arguments, but got 3.
+)
