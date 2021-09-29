0182e756b83a395070e6cfd4fa710015ce9393a1
assu10 <juhyun10.lee@gmail.com>
2021-09-29 11:16:51 +0900

    Typescript - Promise, async/await -  3.5. async 함수 예외 처리

--- chap07/src/index.ts
+++ chap07/src/index.ts
@@ -1,8 +1,27 @@
-// async 함수가 반환하는 값 의미
+// async 함수 예외 처리
 
-const asyncReturn = async() => {
-    return [1, 2, 3]
+
+// -----  async 예외 처리
+const asyncException = async() => {
+    throw new Error('error~');
 };
 
-asyncReturn()
-    .then(value => console.log(value)); // [ 1, 2, 3 ]
+// 프로그램이 비정상 종료됨
+//asyncException();
+
+// 프로그램이 비정상 종료되지 않음
+asyncException()
+    .catch(err => console.log('error:', err.message));  // error: error~
+
+
+// ----- await 예외 처리
+const awaitReject = async() => {
+    await Promise.reject(new Error('error~~'));
+};
+
+// 프로그램이 비정상 종료됨
+//awaitReject();
+
+// 프로그램이 비정상 종료되지 않음
+awaitReject()
+    .catch(err => console.log('error:', err.message));  // error: error~~
