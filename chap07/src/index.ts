fc52300c49a84546c0a2971c6ebbb7db4cce625e
assu10 <juhyun10.lee@gmail.com>
2021-09-29 11:45:06 +0900

    Typescript - Promise, async/await -  async 함수와 Promise.all

--- chap07/src/index.ts
+++ chap07/src/index.ts
@@ -1,27 +1,19 @@
-// async 함수 예외 처리
+// async 함수와 Promise.all
 
+import {readFilePromise} from "./readFilePromise";
 
-// -----  async 예외 처리
-const asyncException = async() => {
-    throw new Error('error~');
+const readFilesAll = async (filenames: string[]) => {
+    // 2. Promise.all 메서드를 사용하여 Promise[] 타입 객체를 단일 Promise 객체로 만듬
+    // 3. 단일 Promise 객체에 await 구문을 적용하여 resolve 된 결과값 반환
+    return await Promise.all(
+        // 1. filenames 에 담긴 string[] 타입 배열에 map 메서드를 적용하여 Promise[] 타입 객체로 전환
+        filenames.map(filename => readFilePromise(filename))
+    )
 };
 
-// 프로그램이 비정상 종료됨
-//asyncException();
-
-// 프로그램이 비정상 종료되지 않음
-asyncException()
-    .catch(err => console.log('error:', err.message));  // error: error~
-
-
-// ----- await 예외 처리
-const awaitReject = async() => {
-    await Promise.reject(new Error('error~~'));
-};
-
-// 프로그램이 비정상 종료됨
-//awaitReject();
-
-// 프로그램이 비정상 종료되지 않음
-awaitReject()
-    .catch(err => console.log('error:', err.message));  // error: error~~
+readFilesAll(['./package.json', './tsconfig.json'])
+    .then(([packageJson, tsconfigJson]: string[]) => {
+        console.log('package.json', packageJson)
+        console.log('tsconfig.json', tsconfigJson)
+    })
+    .catch(err => console.log('error:', err.message));
