a0c46542140c9fc2dada75c8530abd2d7b336461
assu10 <juhyun10.lee@gmail.com>
2021-09-30 10:54:01 +0900

    Typescript - ramda 라이브러리 (1)  - 4.1. 선언형 프로그래밍

--- chap09/src/index.ts
+++ chap09/src/index.ts
@@ -1,10 +1,20 @@
+// R.tap 디버깅 함수로 배열 전/후 값 출력
+
 //import { range } from "ramda";
 import * as R from 'ramda'
 
-const originalArray: number[] = [1, 2, 3];
+const numbers: number[] = R.range(1, 9+1);
 
-const resultArray: number[] = R.pipe(
-    R.map(R.add(1))
-)(originalArray);
+const incNumbers = R.pipe(
+    R.tap(a => console.log('before inc:', a)),  // "strictFunctionTypes": false, 이게 아니면 TS2769: No overload matches this call.   The last overload gave the following error. 오류 발생
+    R.map(R.inc),
+    R.tap(a => console.log('after inc:', a))
+);
 
-console.log(originalArray, resultArray);    // [ 1, 2, 3 ] [ 2, 3, 4 ]
+const newNumbers: number[] = incNumbers(numbers);
+
+console.log(newNumbers);
+
+/*before inc: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+after inc: [2, 3, 4,  5, 6,7, 8, 9, 10]
+[2, 3, 4,  5, 6, 7, 8, 9, 10]*/
