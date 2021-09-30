7d5b8da42b7b0a7855f91e81d43fe521fe3e691d
assu10 <juhyun10.lee@gmail.com>
2021-09-30 16:50:19 +0900

    Typescript - ramda 라이브러리 (1)  -  5.2. `R.allPass`, `R.anyPass` 로직 함수

--- chap09/src/index.ts
+++ chap09/src/index.ts
@@ -1,28 +1,14 @@
 import * as R from 'ramda'
 
-// 1. 3보다 같거나 큰 수만 필터링
+// x 가 min <= x < max 일 때만 통과
+type NumberToBooleanFunc = (n: number) => boolean;
+const selectRange = (min: number, max: number): NumberToBooleanFunc =>
+    R.allPass([
+        R.lte(min),
+        R.gt(max)
+    ]);
+
 R.pipe(
-    R.filter(R.lte(3)),
-    R.tap(n => console.log(n))  // [3, 4, 5,  6, 7, 8, 9, 10]
-)(R.range(1, 10+1));
-
-// 위 코드와 동일한 결과
-R.pipe(
-    R.filter(R.flip(R.gte)(3)),
-    R.tap(n => console.log(n))  // [3, 4, 5,  6, 7, 8, 9, 10]
-)(R.range(1, 10+1));
-
-
-// 2. 7보다 작은 수만 필터링
-R.pipe(
-    R.filter(R.gt(7)),
-    R.tap(n => console.log(n))  // [ 1, 2, 3, 4, 5, 6 ]
-)(R.range(1, 10+1));
-
-
-// 3. 3 <= x < 7 범위의 수만 필터링
-R.pipe(
-    R.filter(R.lte(3)),
-    R.filter(R.gt(7)),
-    R.tap(n => console.log(n))  // [ 3, 4, 5, 6 ]
+    R.filter(selectRange(3, 6+1)),
+    R.tap(n => console.log(n))
 )(R.range(1, 10+1));
