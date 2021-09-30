5b374834393ef85bf8f87e14933a831a7d9348e2
assu10 <juhyun10.lee@gmail.com>
2021-09-30 20:06:58 +0900

    Typescript - ramda 라이브러리 (2)  - 2.2. `R.prop`, `R.assoc` 함수

--- chap09/src/index.ts
+++ chap09/src/index.ts
@@ -1,1 +1,22 @@
-console.log(1)
+import * as R from 'ramda'
+import {IPerson, makeRandomIPerson} from "./model/person";
+
+// R.prop
+const person: IPerson = makeRandomIPerson();
+
+const name = R.pipe(
+    R.prop('name'),
+    R.tap(name => console.log(name))    // Brian Turner
+)(person);
+
+
+// R.assoc
+const person2: IPerson = makeRandomIPerson();
+const getName = R.pipe(
+    R.prop('name'),
+    R.tap(name => console.log(name))
+);
+const originalName = getName(person2);
+
+const modifiedPerson = R.assoc('name', 'assu')(person2);
+const modifiedName = getName(modifiedPerson);
