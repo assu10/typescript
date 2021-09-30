dca0036d365ff966300959b2d2aa6bc4bd555d77
assu10 <juhyun10.lee@gmail.com>
2021-09-30 21:36:33 +0900

    Typescript - ramda 라이브러리 (2)  - 3.4. `R.mergeLeft`, `R.mergeRight` 함수

--- chap09/src/index.ts
+++ chap09/src/index.ts
@@ -1,33 +1,8 @@
 import * as R from 'ramda'
-import {IPerson, makeRandomIPerson} from "./model/person";
 
-const originalPerson: IPerson = makeRandomIPerson();
-const keys: string[] = R.keys(originalPerson);
-const values: any[] = R.values(originalPerson);
+const left: object = {name: 'assu'};
+const right: object = {name: 'jhlee', age: 20}
 
-const zippedPerson: IPerson = R.zipObj(keys, values) as IPerson;
+const merge: object = R.mergeLeft(left, right);
 
-console.log('originalPerson: ', originalPerson, 'zippedPerson: ', zippedPerson);
-/*
-originalPerson:  {
-    name: 'Bertha Gonzalez',
-        age: 64,
-        title: 'Producer',
-        location: {
-        country: 'BQ',
-            city: 'Fifcuwusu',
-            address: '381 Vigfor Loop',
-            coordinates: { latitude: -54.04413, longitude: 56.24533 }
-    }
-} zippedPerson:  {
-    name: 'Bertha Gonzalez',
-        age: 64,
-        title: 'Producer',
-        location: {
-        country: 'BQ',
-            city: 'Fifcuwusu',
-            address: '381 Vigfor Loop',
-            coordinates: { latitude: -54.04413, longitude: 56.24533 }
-    }
-}
-*/
+console.log(merge); // { name: 'assu', age: 20 }
