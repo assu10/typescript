5ecf23ae5b6a8b036bdc28494c1342fdf94a444e
assu10 <juhyun10.lee@gmail.com>
2021-09-30 21:44:45 +0900

    Typescript - ramda 라이브러리 (2)  - 3.5. `R.mergeDeepLeft`, `R.mergeDeepRight` 함수

--- chap09/src/index.ts
+++ chap09/src/index.ts
@@ -1,8 +1,37 @@
 import * as R from 'ramda'
+import {IPerson, makeRandomIPerson} from "./model/person";
+import {ILocation, makeRandomILocation} from "./model/location";
+import {ICoordinates, makeRandomICoordinates} from "./model/coordinates";
 
-const left: object = {name: 'assu'};
-const right: object = {name: 'jhlee', age: 20}
+const person: IPerson = makeRandomIPerson();
+const location: ILocation = makeRandomILocation();
+const coordinates: ICoordinates = makeRandomICoordinates();
 
-const merge: object = R.mergeLeft(left, right);
+const newLocation = R.mergeDeepRight(location, {coordinates});
+const newPerson = R.mergeDeepRight(person, {location: newLocation});
 
-console.log(merge); // { name: 'assu', age: 20 }
+console.log('person', person);
+console.log('newPerson', newPerson);
+/*
+person {
+    name: 'Mildred Hart',
+        age: 21,
+        title: 'Novelist',
+        location: {
+        country: 'TM',
+            city: 'Puafjev',
+            address: '1834 Hijoma Street',
+            coordinates: { latitude: 43.8177, longitude: 138.68656 }
+    }
+}
+newPerson {
+    name: 'Mildred Hart',
+        age: 21,
+        title: 'Novelist',
+        location: {
+        country: 'AM',
+            city: 'Fijosrid',
+            address: '1230 Ekita River',
+            coordinates: { latitude: 36.94358, longitude: -121.11057 }
+    }
+}*/
