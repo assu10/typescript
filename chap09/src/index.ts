f2146d551b213304aba1ede21ae6b258cb14aad8
assu10 <juhyun10.lee@gmail.com>
2021-09-30 22:49:05 +0900

    Typescript - ramda 라이브러리 (2)  - 4.5. `R.sortBy` 함수

--- chap09/src/index.ts
+++ chap09/src/index.ts
@@ -1,10 +1,29 @@
 import * as R from 'ramda'
+import {IPerson, makeRandomIPerson} from "./model/person";
 
-type voidToNumberFunc = () => number;
-const makeRandomNumber = (max: number): voidToNumberFunc =>
-    (): number => Math.floor(Math.random() * max);
+const displayPersons = (prefix: string) => R.pipe(
+    R.map((person: IPerson) => ({name: person.name, age: person.age})),
+    R.tap(o => console.log(prefix, o))
+)
 
-const array = R.range(1, 5+1).map(makeRandomNumber(100));
-const sortedArray = R.sort((a: number, b: number): number => a-b)(array);
+const person: IPerson[] = R.range(1, 4+1).map(makeRandomIPerson);   // person 객체 4개가 있는 하나의 배열 생성
+//console.log(person);
+const nameSortedPersons = R.sortBy(R.prop('name'))(person);
+const ageSortedPersons = R.sortBy(R.prop('age'))(person);
 
-console.log(array, sortedArray);    // [ 9, 54, 43, 28, 11 ] [ 9, 11, 28, 43, 54 ]
+displayPersons('sorted by name: ')(nameSortedPersons);
+displayPersons('sorted by age: ')(ageSortedPersons);
+/*
+sorted by name:  [
+    { name: 'Bruce Floyd', age: 21 },
+    { name: 'Roger Brock', age: 21 },
+    { name: 'Todd Webster', age: 32 },
+    { name: 'Trevor Gardner', age: 24 }
+]
+sorted by age:  [
+    { name: 'Roger Brock', age: 21 },
+    { name: 'Bruce Floyd', age: 21 },
+    { name: 'Trevor Gardner', age: 24 },
+    { name: 'Todd Webster', age: 32 }
+]
+*/
