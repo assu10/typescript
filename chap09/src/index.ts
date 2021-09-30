e61054d1e31d1bde3cd2db00d46a6f7e9fecc328
assu10 <juhyun10.lee@gmail.com>
2021-09-30 22:09:34 +0900

    Typescript - ramda 라이브러리 (2)  - 4.2. `R.flatten` 함수

--- chap09/src/index.ts
+++ chap09/src/index.ts
@@ -1,37 +1,12 @@
 import * as R from 'ramda'
-import {IPerson, makeRandomIPerson} from "./model/person";
-import {ILocation, makeRandomILocation} from "./model/location";
-import {ICoordinates, makeRandomICoordinates} from "./model/coordinates";
 
-const person: IPerson = makeRandomIPerson();
-const location: ILocation = makeRandomILocation();
-const coordinates: ICoordinates = makeRandomICoordinates();
+const array = R.range(1, 2+1).map((x: number) => {
+    return R.range(1, 2+1).map((y: number) => {
+        return [x, y];
+    });
+});
 
-const newLocation = R.mergeDeepRight(location, {coordinates});
-const newPerson = R.mergeDeepRight(person, {location: newLocation});
+console.log(array); // [ [ [ 1, 1 ], [ 1, 2 ] ], [ [ 2, 1 ], [ 2, 2 ] ] ]
 
-console.log('person', person);
-console.log('newPerson', newPerson);
-/*
-person {
-    name: 'Mildred Hart',
-        age: 21,
-        title: 'Novelist',
-        location: {
-        country: 'TM',
-            city: 'Puafjev',
-            address: '1834 Hijoma Street',
-            coordinates: { latitude: 43.8177, longitude: 138.68656 }
-    }
-}
-newPerson {
-    name: 'Mildred Hart',
-        age: 21,
-        title: 'Novelist',
-        location: {
-        country: 'AM',
-            city: 'Fijosrid',
-            address: '1230 Ekita River',
-            coordinates: { latitude: 36.94358, longitude: -121.11057 }
-    }
-}*/
+const flatArray = R.flatten(array);
+console.log(flatArray); // [1, 1, 1, 2, 2, 1, 2, 2]
