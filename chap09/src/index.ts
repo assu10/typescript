b6dec416243b0c62954a4443546a9428f40c44e3
assu10 <juhyun10.lee@gmail.com>
2021-09-30 20:35:23 +0900

    Typescript - ramda 라이브러리 (2)  - 2.4. `R.view`, `R.set`, `R.over` 함수

--- chap09/src/index.ts
+++ chap09/src/index.ts
@@ -1,22 +1,79 @@
 import * as R from 'ramda'
 import {IPerson, makeRandomIPerson} from "./model/person";
 
-// R.prop
+const makeLens = (propName: string) => R.lens(R.prop(propName), R.assoc(propName));
+
+const getter = (lens) => R.view(lens);
+const setter = (lens) => <T>(newValue: T) => R.set(lens, newValue);
+const setterUsingFunc = (lens) => <T, R>(func: (T) => R) => R.over(lens, func);
+
+// 활용
+const nameLens = makeLens('name');
+console.log('nameLens', nameLens);  // [Function (anonymous)]
+
+const getName = getter(nameLens);
+console.log('getName', getName);   // [Function: f1]
+
+const setName = setter(nameLens);
+console.log('nameLens', nameLens);  // [Function (anonymous)]
+
+const setNameUsingFunc = setterUsingFunc(nameLens);
+console.log('setNameUsingFunc', setNameUsingFunc);  // [Function (anonymous)]
+
 const person: IPerson = makeRandomIPerson();
 
-const name = R.pipe(
-    R.prop('name'),
-    R.tap(name => console.log(name))    // Brian Turner
-)(person);
+const name = getName(person);
+console.log('name', name);  // Ina White
 
+const newPerson = setName('assu')(person);
+console.log('newPerson', newPerson);
+/*
+newPerson {
+    name: 'assu',
+        age: 64,
+        title: 'Buyer',
+        location: {
+        country: 'NF',
+            city: 'Jeicfa',
+            address: '1982 Ugeka Mill',
+            coordinates: { latitude: -48.87136, longitude: -164.53377 }
+    }
+}
+*/
 
-// R.assoc
-const person2: IPerson = makeRandomIPerson();
-const getName = R.pipe(
-    R.prop('name'),
-    R.tap(name => console.log(name))
-);
-const originalName = getName(person2);
+const anotherPerson = setNameUsingFunc(name => `Miss ${name}`)(person);
+console.log('anotherPerson', anotherPerson);
+/*
+anotherPerson {
+    name: 'Miss Lura Cole',
+        age: 56,
+        title: 'EEO Compliance Manager',
+        location: {
+        country: 'LK',
+            city: 'Mutubrew',
+            address: '966 Boge Grove',
+            coordinates: { latitude: 29.59275, longitude: -19.68588 }
+    }
+}
+*/
 
-const modifiedPerson = R.assoc('name', 'assu')(person2);
-const modifiedName = getName(modifiedPerson);
+const capitalPerson = setNameUsingFunc(R.toUpper)(person);
+console.log('capitalPerson', capitalPerson);
+/*
+capitalPerson {
+    name: 'LIDA OWEN',
+        age: 35,
+        title: 'Manpower Planner',
+        location: {
+        country: 'TN',
+            city: 'Ekapuvzo',
+            address: '1286 Zoklah Court',
+            coordinates: { latitude: 28.94433, longitude: 177.0845 }
+    }
+}
+*/
+
+console.log(
+    name, getName(newPerson), getName(anotherPerson), getName(capitalPerson)
+)
+// Rose Gibson assu Miss Rose Gibson ROSE GIBSON
