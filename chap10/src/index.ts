a2c648a4ecf07e5bbad7e99434b4bdbbd86f9544
assu10 <juhyun10.lee@gmail.com>
2021-10-01 16:24:15 +0900

    Typescript - Generic 프로그래밍  - 2. 제네릭 타입 제약

--- chap10/src/index.ts
+++ chap10/src/index.ts
@@ -11,8 +11,12 @@ class Valuable<T> implements IValuable<T> {
 // 제네릭 함수는 아래처럼 자신의 타입변수 T 를 제네릭 인터페이스의 타입 변수 쪽으로 넘기는 형태로 구현
 const printValue = <T>(o: IValuable<T>): void => console.log(o.value);
 
-// printValue 함수는 아래처럼 다양한 타입을 대상으로 동작한다.
-printValue(new Valuable<boolean>(true));
-printValue(new Valuable<number[]>([1,2,3]));
-printValue(new Valuable(1));        // 타입 변수를 생략하면 스스로 추론
-printValue(new Valuable('test'));
+// 제네릭 타입 제약 구문
+const printValueT = <Q, T extends IValuable<Q>>(o: T) => console.log(o.value);
+
+// 매개변수 타입을 어떤 방식으로 제약하느냐만 다르고 사용법은 완전히 동일함
+printValue(new Valuable(1));
+printValueT(new Valuable(1));
+
+printValue({value: true});
+printValueT({value: true});
