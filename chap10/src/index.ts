49aa151aaef4e88dfaf2911bddf5cd545c8fbfb0
assu10 <juhyun10.lee@gmail.com>
2021-10-01 16:59:48 +0900

    Typescript - Generic 프로그래밍  - 2.1. `new` 타입 제약

--- chap10/src/index.ts
+++ chap10/src/index.ts
@@ -1,22 +1,17 @@
-// 제네릭 인터페이스 정의
-interface IValuable<T> {
-    value: T
+// TS2351: This expression is not constructable.   Type 'unknown' has no construct signatures.
+//const create = <T>(type: T): T => new type();
+
+const create = <T extends {new(): T}>(type: T): T => new type();
+
+const create2 = <T>(type: new() => T): T => new type();
+
+const create3 = <T>(type: {new(...args): T}, ...args):T => new type(...args);
+
+class Point {
+    constructor(public x: number, public y: number) { }
 }
 
-// 제네릭 인터페이스를 구현하는 제네릭 클래스는 아래 constructor() 처럼 자신이 가진 타입변수 T 를 인터페이스 쪽 제네릭 타입 변수로 넘길 수 있다.
-class Valuable<T> implements IValuable<T> {
-    constructor(public value: T) { }
-}
-
-// 제네릭 함수는 아래처럼 자신의 타입변수 T 를 제네릭 인터페이스의 타입 변수 쪽으로 넘기는 형태로 구현
-const printValue = <T>(o: IValuable<T>): void => console.log(o.value);
-
-// 제네릭 타입 제약 구문
-const printValueT = <Q, T extends IValuable<Q>>(o: T) => console.log(o.value);
-
-// 매개변수 타입을 어떤 방식으로 제약하느냐만 다르고 사용법은 완전히 동일함
-printValue(new Valuable(1));
-printValueT(new Valuable(1));
-
-printValue({value: true});
-printValueT({value: true});
+[
+    create3(Date),  // 2021-10-01T07:57:56.458Z
+    create3(Point, 0, 0)    // Point { x: 0, y: 0 }
+].forEach((s) => console.log(s));
