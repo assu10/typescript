d7f56929de04ee138d2e4d7db93afcfa7a00f640
assu10 <juhyun10.lee@gmail.com>
2021-10-01 13:28:09 +0900

    Typescript - ramda 라이브러리 (2)  - 5.4. `R.identity` 조합자

--- chap09/src/index.ts
+++ chap09/src/index.ts
@@ -1,22 +1,17 @@
 import * as R from 'ramda'
-import {IPerson, makeRandomIPerson} from "./model/person";
 
-const displayPersons = (prefix: string) => R.pipe(
-    R.map((person: IPerson) => ({name: person.name, age: person.age})),
-    R.tap(o => console.log(prefix, o))
-) as any
+type NumberToNumFunc = (n: number) => number;
 
-const person: IPerson[] = R.range(1, 4+1).map(makeRandomIPerson);   // person 객체 4개가 있는 하나의 배열 생성
-const nameSortedPersons = R.sortWith([
-    R.descend(R.prop('name'))
-])(person);
+const applyDiscount = (minimum: number, discount: number): NumberToNumFunc =>
+    R.pipe(
+        R.ifElse(
+            R.flip(R.gte)(minimum),
+            R.flip(R.subtract)(discount),
+            R.identity
+        ),
+        R.tap(amount => console.log(amount))
+    );
+const calcPrice = applyDiscount(5000, 500);
 
-displayPersons('sorted by name: ')(nameSortedPersons);
-/*
-sorted by name:  [
-    { name: 'Lilly McKinney', age: 51 },
-    { name: 'Josie Morrison', age: 61 },
-    { name: 'Flora Manning', age: 61 },
-    { name: 'Fanny Griffith', age: 63 }
-]
-*/
+const discountedPrice = calcPrice(6000);    // 5500
+const notDiscountedPrice = calcPrice(4500); // 4500
