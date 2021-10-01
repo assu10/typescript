6607eac7eaf4670cddc2819f744fb760f6004aa0
assu10 <juhyun10.lee@gmail.com>
2021-10-01 14:08:26 +0900

    Typescript - ramda 라이브러리 (2)  - 5.7. `R.ap` 조합자

--- chap09/src/index.ts
+++ chap09/src/index.ts
@@ -1,13 +1,11 @@
 import * as R from 'ramda'
 
-// 함수 T 는 value 값을 첫 번째 매개변수로 받는 2차 고차함수
-const T = value => R.pipe(
-    R.applyTo(value),
-    R.tap(value => console.log(value))
+const repeat = (n, cb) => R.range(1, n+1).map(n => cb);
+
+const callAndAppend = R.pipe(
+    R.ap(repeat(3, R.identity)),
+    R.tap(a => console.log(a))
 );
 
-// value100 은 첫 번째 매개변수에 100을 대입해서 만든 1차 함수로 R.identity 처럼 매개변수가 한 개인 콜백함수를 입력받을 수 있음
-const value100 = T(100);
-
-const sameValue = value100(R.identity);    // 100
-const add1Value = value100(R.add(1));   // 101
+const input = [1,2,3];
+const result = callAndAppend(input);    // [1, 2, 3, 1, 2, 3, 1, 2, 3]
