b088507b9e1969166e5e4b45140259aabe1ffebc
assu10 <juhyun10.lee@gmail.com>
2021-10-02 19:15:42 +0900

    Typescript - Generic 프로그래밍  - 3.3. 합집합 타입 구분하기

--- chap10/src/index.ts
+++ chap10/src/index.ts
@@ -1,25 +1,41 @@
 interface ISquare {
+    tag: 'square',
     size: number
 }
+
 interface IRectangle {
+    tag: 'rectangle',
     width: number,
     height: number
 }
 interface ICircle {
+    tag: 'circle',
     radius: number
 }
 
 // 위 인터페이스 타입으로 만든 객체들
-const square: ISquare = {size: 10};
-const ractangle: IRectangle = {width: 4, height: 5};
-const circle: ICircle = {radius: 10}
+const square: ISquare = {tag: 'square', size: 10};
+const ractangle: IRectangle = {tag: 'rectangle', width: 4, height: 5};
+const circle: ICircle = {tag: 'circle', radius: 10}
 
 // 면적 계산을 하기 위한 매개변수의 타입
 type IShape = ISquare | IRectangle | ICircle;
 
 // 면적을 계산하는 함수
 const calcArea = (shape: IShape): number => {
-    // shape 객체가 ISquare 인지 IRectangle 인지 알 수가 없어서 면적을 계산할 수 없음
-
+    switch (shape.tag) {
+        case 'square':
+            return shape.size * shape.size;
+        case 'rectangle':
+            return shape.width * shape.height;
+        case 'circle':
+            return Math.PI * shape.radius * shape.radius;
+    }
     return 0;
 }
+
+console.log(
+    calcArea(square),   // 100
+    calcArea(ractangle),    // 20
+    calcArea(circle)    // 314.1592653589793
+)
