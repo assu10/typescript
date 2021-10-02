428378552b88b81c21599c779a019ca06adee38c
assu10 <juhyun10.lee@gmail.com>
2021-10-02 19:37:14 +0900

    Typescript - Generic 프로그래밍  - 4.2. 타입 가드 (type guard)

--- chap10/src/index.ts
+++ chap10/src/index.ts
@@ -12,9 +12,9 @@ class Fish {
 // 함수
 const flyOrSwim = (o: Bird | Fish): void => {
     if (o instanceof Bird) {
-        (o as Bird).fly();
+        o.fly();
     } else if (o instanceof Fish) {
-        (<Fish>o).swim();
+        o.swim();
     }
 }
 
