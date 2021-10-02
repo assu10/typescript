85bbaed7ca4dc47e9cd25d8e5c35d19e3641ba6d
assu10 <juhyun10.lee@gmail.com>
2021-10-02 18:23:34 +0900

    Typescript - Generic 프로그래밍  - 3.1. 합집합 타입 (`|`)

--- chap10/src/index.ts
+++ chap10/src/index.ts
@@ -1,22 +1,5 @@
-const obj = {name: 'assu', age: 20, city: 'seoul', country: 'korea'};
+type NumberOrString = number | string;
 
-/*const pick = (obj, keys) => keys.map(key => ({[key]: obj[key]}))
-    .reduce((result, value) => ({...result, ...value}), {});
-
-console.log(
-    pick(obj, ['name', 'age']), // { name: 'assu', age: 20 }
-    pick(obj, ['nam', 'agge'])  // { nam: undefined, agge: undefined }
-)*/
-
-// TS2536: Type 'K' cannot be used to index type 'T'.
-/*
-const pick = <T, K>(obj: T, keys: K[]) => keys.map(key => ({[key]: obj[key]}))
-.reduce((result, value) => ({...result, ...value}), {});*/
-
-const pick = <T, K extends keyof T>(obj: T, keys: K[]) =>
-    keys.map(key => ({[key]: obj[key]}))
-        .reduce((result, value) => ({...result, ...value}), {});
-
-console.log(
-    pick(obj, ['name', 'age']), // { name: 'assu', age: 20 }
-    pick(obj, ['nam', 'agge'])  // TS2322: Type '"agge"' is not assignable to type '"name" | "age" | "city" | "country"'.
+let ns: NumberOrString = 1;
+ns = 'hello'
+ns = true;  // TS2322: Type 'boolean' is not assignable to type 'NumberOrString'.
