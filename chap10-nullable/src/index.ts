f31555b0fdb620eb2265830bf71584e674471ff7
assu10 <juhyun10.lee@gmail.com>
2021-10-02 23:51:04 +0900

    Typescript - Generic 프로그래밍  - 6.3. 옵션 체이닝 연산자 `?.`

--- chap10-nullable/src/index.ts
+++ chap10-nullable/src/index.ts
@@ -1,8 +1,12 @@
-interface IPerson {
-    name: string,
-    age?: number
-}
+type ICoodinates = {longitude: number}
+type ILocation = {country: string, coords?: ICoodinates}
+type IPerson = {name: string, location?: ILocation}
 
-let person: IPerson;
-//console.log(person.name); // TS2454: Variable 'person' is used before being assigned.
-console.log(person?.name);
+let person: IPerson = {name: 'assu'}
+let longitude = person?.location?.coords?.longitude;
+console.log(longitude); // undefined
+
+if (person && person.location && person.location.coords) {
+    longitude = person.location.coords.longitude
+}
+console.log(longitude)  // undefined
