714936447db4db14d986db4b6bcbc1972d9d6242
assu10 <juhyun10.lee@gmail.com>
2021-10-02 18:56:59 +0900

    Typescript - Generic 프로그래밍  - 3.3. 합집합 타입 구분하기

--- chap10/src/index.ts
+++ chap10/src/index.ts
@@ -1,9 +1,25 @@
-// 타입 T 와 U 객체를 결합하여 타입이 T & U 인 새로운 객체를 만드는 예시
-const mergeObjects = <T, U>(a: T, b: U): T & U => ({...a, ...b});
+interface ISquare {
+    size: number
+}
+interface IRectangle {
+    width: number,
+    height: number
+}
+interface ICircle {
+    radius: number
+}
 
-type INameable = {name: string};
-type IAgeable = {age: number};
+// 위 인터페이스 타입으로 만든 객체들
+const square: ISquare = {size: 10};
+const ractangle: IRectangle = {width: 4, height: 5};
+const circle: ICircle = {radius: 10}
 
-const nameAndAge: INameable & IAgeable = mergeObjects({name: 'assu'}, {age: 20});
+// 면적 계산을 하기 위한 매개변수의 타입
+type IShape = ISquare | IRectangle | ICircle;
 
-console.log(nameAndAge);    // { name: 'assu', age: 20 }
+// 면적을 계산하는 함수
+const calcArea = (shape: IShape): number => {
+    // shape 객체가 ISquare 인지 IRectangle 인지 알 수가 없어서 면적을 계산할 수 없음
+
+    return 0;
+}
