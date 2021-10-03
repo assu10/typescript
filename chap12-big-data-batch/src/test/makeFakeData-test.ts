2da8e22dc9bd69f936d1417b8047c7d47ccd3d2a
assu10 <juhyun10.lee@gmail.com>
2021-10-03 23:00:05 +0900

    Typescript - 빅데이터 배치 프로그램  - 5. 가짜 데이터 생성

---
+++ chap12-big-data-batch/src/test/makeFakeData-test.ts
@@ -0,0 +1,13 @@
+import {makeFakeData, IFake} from "../fake/makeFakeData";
+
+const fakeData: IFake = makeFakeData();
+console.log(fakeData);
+/*
+{
+    name: 'Ida Fuller',
+        email: 'nap@huc.sv',
+    profession: 'City Manager',
+    birthday: 1967-02-12T00:30:53.300Z,
+    sentence: 'Li po vevmad getire modde lu fekural ig if fimo wocef kisodcil famateme.'
+}
+*/
